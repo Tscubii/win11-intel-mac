f3f363f86b0388bc3d7fd3b179577d3775bc1a52
Tscubii <mrscoobosalt@gmail.com>
2026-01-06 23:11:21 -0500

    Update WinPE-Helper.bat

--- assets/WinPE-Helper.bat
+++ assets/WinPE-Helper.bat
@@ -17,12 +17,12 @@ if defined DISK_LETTER (
     
     if !errorlevel! equ 0 (
         if defined USB_LETTER (
-            md %DISK_LETTER%:\mount
-            dism /Mount-Image /ImageFile:%USB_LETTER%:\sources\boot.wim /Index:2 /MountDir:%DISK_LETTER%:\mount
+            md %DISK_LETTER%:\mnt
+            dism /Mount-Image /ImageFile:%USB_LETTER%:\sources\boot.wim /Index:2 /MountDir:%DISK_LETTER%:\mnt
             
             if !errorlevel! equ 0 (
-                dism /Image:%DISK_LETTER%:\mount /Add-Driver /Driver:%USB_LETTER%:\Drivers /Recurse
-                dism /Unmount-Image /MountDir:%DISK_LETTER%:\mount /Commit
+                dism /Image:%DISK_LETTER%:\mnt /Add-Driver /Driver:%USB_LETTER%:\WinPEDriver /Recurse
+                dism /Unmount-Image /MountDir:%DISK_LETTER%:\mnt /Commit
                 
                 echo Restarting in 10 seconds... (Press any key to restart now.)
                 timeout /t 10 >nul
