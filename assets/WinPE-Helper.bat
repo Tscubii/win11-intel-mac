4e0b6b8186d737f84fef512407b1710774ef7674
Tscubii <mrscoobosalt@gmail.com>
2026-01-07 01:06:34 -0500

    Update WinPE-Helper.bat

--- assets/WinPE-Helper.bat
+++ assets/WinPE-Helper.bat
@@ -4,6 +4,10 @@ setlocal enabledelayedexpansion
 set "DISK_LABEL=BOOTCAMP"
 set "USB_LABEL=Win11"
 
+echo If the WinPE drivers haven't been installed, wait 20 seconds. Otherwise, press any key to skip.
+timeout /t 20 >nul
+if ERRORLEVEL 1 ( goto END )
+
 for %%a in (C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
     vol %%a: 2>nul | find /i "%DISK_LABEL%" >nul
     if !errorlevel! equ 0 set "DISK_LETTER=%%a"
@@ -32,3 +36,5 @@ if defined DISK_LETTER (
         )
     )
 )
+
+:END
