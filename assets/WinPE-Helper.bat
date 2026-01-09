938842c0e54f32731b192e32b9b61c75b3973d79
Tscubii <mrscoobosalt@gmail.com>
2026-01-09 00:48:04 -0500

    Update WinPE-Helper.bat

--- assets/WinPE-Helper.bat
+++ assets/WinPE-Helper.bat
@@ -4,51 +4,43 @@ setlocal enabledelayedexpansion
 set "DISK_LABEL=BOOTCAMP"
 set "USB_LABEL=Win11"
 
-echo "Looking for the drives of %DISK_LABEL% and %USB_LABEL%..."
+echo Finding the drives of %DISK_LABEL% and %USB_LABEL%...
 
 for %%i in (C D E F G H I J K L M N O P Q R S T U V W Y Z) do (
     vol %%i: 2>nul | find /i "%DISK_LABEL%" >nul
     if !ERRORLEVEL! equ 0 (
-        echo "%DISK_LABEL% is drive %%i:!"
+        echo %DISK_LABEL% is drive %%i:!
         set "DISK_LETTER=%%i"
-    ) else (
-        echo "Couldn't find the drive of %DISK_LABEL%!"
     )
     
     vol %%i: 2>nul | find /i "%USB_LABEL%" >nul
     if !ERRORLEVEL! equ 0 (
-        echo "%USB_LABEL% is drive %%i:!"
+        echo %USB_LABEL% is drive %%i:!
         set "USB_LETTER=%%i"
-    ) else (
-        echo "Couldn't find the drive of %USB_LABEL%!"
     )
 )
 
 if defined DISK_LETTER (
-    echo "Formatting %DISK_LABEL% (%DISK_LETTER%:) to NTFS..."
+    echo Formatting %DISK_LABEL% (%DISK_LETTER%:) to NTFS...
     format %DISK_LETTER%: /fs:ntfs /v:BOOTCAMP /q /y
     
     if !ERRORLEVEL! equ 0 (
         if defined USB_LETTER (
-            echo "Mounting Windows Setup..."
+            echo Mounting Windows Setup...
             md %DISK_LETTER%:\Temp
             dism /Mount-Image /ImageFile:%USB_LETTER%:\sources\boot.wim /Index:2 /MountDir:%DISK_LETTER%:\Temp
             
             if !ERRORLEVEL! equ 0 (
-                echo "Adding drivers to Windows Setup..."
+                echo Adding drivers to Windows Setup...
                 dism /Image:%DISK_LETTER%:\Temp /Add-Driver /Driver:%USB_LETTER%:\WinPEDriver /Recurse
                 
-                echo "Unmounting Windows Setup..."
+                echo Saving Windows Setup...
                 dism /Unmount-Image /MountDir:%DISK_LETTER%:\Temp /Commit
                 rd %DISK_LETTER%:\Temp
                 
-                echo "Windows Setup will now restart!"
+                echo Windows Setup will now restart!
                 wpeutil reboot
-            ) else (
-                echo "Couldn't mount Windows Setup!"
-            )
-        )
-    ) else (
-        echo "Couldn't format %DISK_LABEL% (%DISK_LETTER%:) to NTFS!"
-    )
-)
+            ) else ( echo Couldn't mount Windows Setup! )
+        ) else ( echo Couldn't find the drive of %USB_LABEL%! )
+    ) else ( echo Couldn't format %DISK_LABEL% (%DISK_LETTER%:) to NTFS! )
+) else ( echo Couldn't find the drive of %DISK_LABEL%! )
