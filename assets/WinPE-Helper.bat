765eef882d4ec02dafd3e72a2eeb88e02a8cbbdd
Tscubii <mrscoobosalt@gmail.com>
2026-01-08 23:29:20 -0500

    Update WinPE-Helper.bat

--- assets/WinPE-Helper.bat
+++ assets/WinPE-Helper.bat
@@ -4,28 +4,51 @@ setlocal enabledelayedexpansion
 set "DISK_LABEL=BOOTCAMP"
 set "USB_LABEL=Win11"
 
+echo "Looking for the drives of %DISK_LABEL% and %USB_LABEL%..."
+
 for %%i in (C D E F G H I J K L M N O P Q R S T U V W Y Z) do (
     vol %%i: 2>nul | find /i "%DISK_LABEL%" >nul
-    if !errorlevel! equ 0 ( set "DISK_LETTER=%%i" )
+    if !ERRORLEVEL! equ 0 (
+        echo "%DISK_LABEL% is drive %%i:!"
+        set "DISK_LETTER=%%i"
+    ) else (
+        echo "Couldn't find the drive of %DISK_LABEL%!"
+    )
     
     vol %%i: 2>nul | find /i "%USB_LABEL%" >nul
-    if !errorlevel! equ 0 ( set "USB_LETTER=%%i" )
+    if !ERRORLEVEL! equ 0 (
+        echo "%USB_LABEL% is drive %%i:!"
+        set "USB_LETTER=%%i"
+    ) else (
+        echo "Couldn't find the drive of %USB_LABEL%!"
+    )
 )
 
 if defined DISK_LETTER (
+    echo "Formatting %DISK_LABEL% (%DISK_LETTER%:) to NTFS..."
     format %DISK_LETTER%: /fs:ntfs /v:BOOTCAMP /q /y
     
-    if !errorlevel! equ 0 (
+    if !ERRORLEVEL! equ 0 (
         if defined USB_LETTER (
+            echo "Mounting Windows Setup..."
             md %DISK_LETTER%:\Temp
             dism /Mount-Image /ImageFile:%USB_LETTER%:\sources\boot.wim /Index:2 /MountDir:%DISK_LETTER%:\Temp
             
-            if !errorlevel! equ 0 (
+            if !ERRORLEVEL! equ 0 (
+                echo "Adding drivers to Windows Setup..."
                 dism /Image:%DISK_LETTER%:\Temp /Add-Driver /Driver:%USB_LETTER%:\WinPEDriver /Recurse
+                
+                echo "Unmounting Windows Setup..."
                 dism /Unmount-Image /MountDir:%DISK_LETTER%:\Temp /Commit
                 rd %DISK_LETTER%:\Temp
+                
+                echo "Windows Setup will now restart!"
                 wpeutil reboot
+            ) else (
+                echo "Couldn't mount Windows Setup!"
             )
         )
+    ) else (
+        echo "Couldn't format %DISK_LABEL% (%DISK_LETTER%:) to NTFS!"
     )
 )
