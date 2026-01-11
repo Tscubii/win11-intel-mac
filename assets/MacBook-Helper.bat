56022c1f3dfcdf68a4ab62bdf2adcd88175a5eac
Tscubii <mrscoobosalt@gmail.com>
2026-01-10 22:39:30 -0500

    Update MacBook-Helper.bat

--- assets/MacBook-Helper.bat
+++ assets/MacBook-Helper.bat
@@ -7,16 +7,14 @@ echo Finding the drive of %USB_LABEL%...
 for %%i in (C D E F G H I J K L M N O P Q R S T U V W Y Z) do (
     vol %%i: 2>nul | find /i "%USB_LABEL%" >nul
     if !ERRORLEVEL! equ 0 (
-        echo %USB_LABEL% is drive %%i
+        echo %USB_LABEL% is drive %%i:
         set "USB_LETTER=%%i"
     )
 )
 
 if defined USB_LETTER (
-    if exist %USB_LETTER%:\WindowsSupport (
-        set "DRV_FOLDER=%USB_LETTER%:\WindowsSupport"
-        
+    if exist %USB_LETTER%:\WindowsSupport\$WinPEDriver$ (
         echo Adding drivers to Windows Setup...
-        for /r %DRV_FOLDER% %%i in (*.inf) do ( drvload %%i )
-    ) else ( echo Couldn't find %USB_LETTER%:\WindowsSupport. )
+        for /r %USB_LETTER%:\WindowsSupport\$WinPEDriver$ %%i in (*.inf) do ( drvload %%i )
+    ) else ( echo Couldn't find %USB_LETTER%:\WindowsSupport\$WinPEDriver$. )
 ) else ( echo Couldn't find the drive of %USB_LABEL%. )
