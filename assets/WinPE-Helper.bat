3aecc3e1e15ab2d193a1e5e0953b07bf42c91f75
Tscubii <mrscoobosalt@gmail.com>
2026-01-16 21:52:02 -0500

    Update WinPE-Helper.bat

--- assets/WinPE-Helper.bat
+++ assets/WinPE-Helper.bat
@@ -2,7 +2,6 @@
 setlocal enabledelayedexpansion
 
 set "USB_LABEL=Win11"
-set "DRV_PATH=WindowsSupport\$WinPEDriver$"
 
 echo Finding %USB_LABEL%'s drive...
 for %%i in (C D E F G H I J K L M N O P Q R S T U V W Y Z) do (
@@ -14,9 +13,8 @@ for %%i in (C D E F G H I J K L M N O P Q R S T U V W Y Z) do (
 )
 
 if defined USB_LETTER (
-    set "DRV_FOLDER=%USB_LETTER%:\%DRV_PATH%"
-    if exist !DRV_FOLDER! (
+    if exist %USB_LETTER%:\WindowsSupport\$WinPEDriver$ (
         echo Adding drivers to Windows Setup...
-        for /r !DRV_FOLDER! %%i in (*.inf) do ( drvload %%i )
-    ) else ( echo Couldn't find !DRV_FOLDER!. )
+        for /r %USB_LETTER%:\WindowsSupport\$WinPEDriver$ %%i in (*.inf) do ( drvload %%i )
+    ) else ( echo Couldn't find %USB_LETTER%:\WindowsSupport\$WinPEDriver$. )
 ) else ( echo Couldn't find %USB_LABEL%'s drive. )
