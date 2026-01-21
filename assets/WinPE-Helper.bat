aa8d69c49f194797ac63912c1786a1b4547516a8
Tscubii <mrscoobosalt@gmail.com>
2026-01-21 11:40:09 -0500

    Update WinPE-Helper.bat

--- assets/WinPE-Helper.bat
+++ assets/WinPE-Helper.bat
@@ -3,18 +3,23 @@ setlocal enabledelayedexpansion
 
 set "USB_LABEL=Win11"
 
-echo Finding %USB_LABEL%'s drive...
+echo Finding the drive of %USB_LABEL%...
 for %%i in (C D E F G H I J K L M N O P Q R S T U V W Y Z) do (
     vol %%i: 2>nul | find /i "%USB_LABEL%" >nul
     if !ERRORLEVEL! equ 0 (
         echo %USB_LABEL% is drive %%i:
-        set "USB_LETTER=%%i"
+        set "USB_LETTER=%%i:"
     )
 )
 
 if defined USB_LETTER (
-    if exist %USB_LETTER%:\WindowsSupport\$WinPEDriver$ (
-        echo Loading drivers into Windows Setup...
-        for /r %USB_LETTER%:\WindowsSupport\$WinPEDriver$ %%i in (*.inf) do ( drvload %%i )
-    ) else ( echo Couldn't find %USB_LETTER%:\WindowsSupport\$WinPEDriver$. )
-) else ( echo Couldn't find %USB_LABEL%'s drive. )
+    if exist %USB_LETTER%\WindowsSupport\$WinPEDriver$ (
+        echo Loading boot-critical drivers into Windows Setup...
+        for /r %USB_LETTER%\WindowsSupport\$WinPEDriver$ %%i in (*.inf) do ( drvload %%i )
+        
+        if exist %USB_LETTER%\WindowsSupport\BootCamp\Drivers\Apple (
+            echo Loading Apple-specific drivers into Windows Setup...
+            for /r %USB_LETTER%\WindowsSupport\BootCamp\Drivers\Apple %%i in (*.inf) do ( drvload %%i )
+        ) else ( echo Could not find %USB_LETTER%\WindowsSupport\BootCamp\Drivers\Apple. )
+    ) else ( echo Could not find %USB_LETTER%\WindowsSupport\$WinPEDriver$. )
+) else ( echo Could not find the drive of %USB_LABEL%. )
