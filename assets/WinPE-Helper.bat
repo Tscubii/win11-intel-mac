1f75c8f0143c1fbaf34be9064182a4d0391a4ffb
Tscubii <mrscoobosalt@gmail.com>
2026-02-01 14:48:49 -0500

    Update WinPE-Helper.bat

--- assets/WinPE-Helper.bat
+++ assets/WinPE-Helper.bat
@@ -15,10 +15,6 @@ for %%i in (C D E F G H I J K L M N O P Q R S T U V W Y Z) do (
 if defined USB_LETTER (
     if exist %USB_LETTER%\WindowsSupport\$WinPEDriver$ (
         echo Loading drivers into Windows Setup...
-        
         for /r %USB_LETTER%\WindowsSupport\$WinPEDriver$\SerialIO %%i in (*.inf) do ( drvload %%i )
-        for /r %USB_LETTER%\WindowsSupport\$WinPEDriver$\AppleDFR %%i in (*.inf) do ( drvload %%i )
-        for /r %USB_LETTER%\WindowsSupport\$WinPEDriver$\AppleMultiTouchTrackPad64 %%i in (*.inf) do ( drvload %%i )
-        for /r %USB_LETTER%\WindowsSupport\$WinPEDriver$\AppleMultiTouchTrackPadPro64 %%i in (*.inf) do ( drvload %%i )
     ) else ( echo Could not find %USB_LETTER%\WindowsSupport\$WinPEDriver$. )
 ) else ( echo Could not find the drive of %USB_LABEL%. )
