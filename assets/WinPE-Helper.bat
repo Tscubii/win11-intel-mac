48c3ec3753044f90d230fb9baa0c91578a236a8a
Tscubii <mrscoobosalt@gmail.com>
2026-07-23 00:38:16 -0400

    Update WinPE-Helper.bat

--- assets/WinPE-Helper.bat
+++ assets/WinPE-Helper.bat
@@ -4,47 +4,53 @@ setlocal enabledelayedexpansion
 set "USB_LABEL=Win11"
 set "HDD_LABEL=BOOTCAMP"
 
-echo Finding the drive of %USB_LABEL%...
+echo Finding %USB_LABEL%...
 for %%i in (C D E F G H I J K L M N O P Q R S T U V W Y Z) do (
     vol %%i: 2>nul | find /i "%USB_LABEL%" >nul
     if !ERRORLEVEL! equ 0 (
-        echo %USB_LABEL% is drive %%i:
+        echo Found %USB_LABEL% (%%i:).
         set "USB_LETTER=%%i:"
     )
 )
+if not defined USB_LETTER ( echo ERROR: Could not find %USB_LABEL%. Cannot continue. & goto SHUTDOWN )
 
-echo Finding the drive of %HDD_LABEL%...
+echo Finding %HDD_LABEL%...
 for %%i in (C D E F G H I J K L M N O P Q R S T U V W Y Z) do (
     vol %%i: 2>nul | find /i "%HDD_LABEL%" >nul
     if !ERRORLEVEL! equ 0 (
-        echo %HDD_LABEL% is drive %%i:
+        echo Found %HDD_LABEL% (%%i:).
         set "HDD_LETTER=%%i:"
     )
 )
+if not defined HDD_LETTER ( echo ERROR: Could not find %HDD_LABEL%. Cannot continue. & goto SHUTDOWN )
 
-if not defined USB_LETTER ( echo ERROR: Could not find the drive of %USB_LABEL%. Cannot continue. & goto SHUTDOWN )
-if not defined HDD_LETTER ( echo ERROR: Could not find the drive of %HDD_LABEL%. Cannot continue. & goto SHUTDOWN )
-
-if not exist "%HDD_LETTER%\Mount" (
+if not exist "%HDD_LETTER%\Mount\Windows" (
     echo Formatting %HDD_LABEL% (%HDD_LETTER%) to NTFS...
-    format %HDD_LETTER% /fs:NTFS /v:%HDD_LABEL% /q /y
+    format %HDD_LETTER% /fs:ntfs /v:%HDD_LABEL% /q /y
     
     echo Creating mount folder...
-    md "%HDD_LETTER%\Mount"
+    if not exist "%HDD_LETTER%\Mount" ( md "%HDD_LETTER%\Mount" )
     
     echo Mounting boot.wim...
     dism /Mount-Image /ImageFile:"%USB_LETTER%\sources\boot.wim" /Index:2 /MountDir:"%HDD_LETTER%\Mount"
+    if !ERRORLEVEL! neq 0 ( echo ERROR: Could not mount boot.wim. Cannot continue. & goto SHUTDOWN )
     
-    echo Adding drivers to boot.wim...
+    echo Adding drivers...
     dism /Image:"%HDD_LETTER%\Mount" /Add-Driver /Driver:"%USB_LETTER%\WindowsSupport\$WinPEDriver$" /Recurse
+    if !ERRORLEVEL! neq 0 (
+        echo ERROR: Could not add drivers. Cannot continue. Discarding...
+        dism /Unmount-Image /MountDir:"%HDD_LETTER%\Mount" /Discard
+        goto SHUTDOWN
+    )
     
-    echo Saving and unmounting boot.wim...
+    echo Saving and unmounting...
     dism /Unmount-Image /MountDir:"%HDD_LETTER%\Mount" /Commit
     
     echo INFO: boot.wim prepared successfully. Boot from USB again to continue.
     goto SHUTDOWN
 ) else (
-    echo INFO: Found mount folder, assuming boot.wim already prepared. Self-deleting...
+    echo INFO: Found boot.wim remnants. Discarding and self-deleting...
+    dism /Unmount-Image /MountDir:"%HDD_LETTER%\Mount" /Discard >nul 2>&1
     goto 2>nul & del "%~f0"
     goto EOF
 )
